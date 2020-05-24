7719045336dacc663656e48be893de2702a2feb5
EduardoJM <eduardo_y05@outlook.com>
2020-05-24 11:33:36 -0300

    feat: add the ElectronLink component used for create links

---
+++ app/core/components/ElectronLink/index.js
@@ -0,0 +1,11 @@
+const { ipcRenderer } = require('electron');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.link[to]');
+    links.forEach((item) => {
+        item.addEventListener('click', () => {
+            const to = item.getAttribute('to');
+            ipcRenderer.send('navigate-to', to);
+        });
+    });
+});
